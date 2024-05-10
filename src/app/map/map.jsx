0e1799a33b93f2b6fa0c1d6f9ffaddd1977d1794
chankk <42552752+chankk@users.noreply.github.com>
2024-05-10 16:30:04 -0600

    refactor map

--- src/app/map/map.jsx
+++ src/app/map/map.jsx
@@ -13,41 +13,10 @@ import FeatureTable from "@arcgis/core/widgets/FeatureTable.js";
 
 config.apiKey = process.env.NEXT_PUBLIC_ARCGIS_KEY;
 
-const fields = [
-	{
-		name: "id",
-		type: "oid",
-	},
-	{
-		name: "name",
-		type: "string",
-	},
-	{
-		name: "address",
-		type: "string",
-	},
-	{
-		name: "locality",
-		type: "string",
-	},
-];
-
-const popupTemplate = {
-	title: "{name}",
-	content: "{address} {locality} AB",
-};
-
-const renderer = {
-	type: "simple",
-	symbol: {
-		type: "simple-marker",
-		color: "#8e5776",
-	},
-};
-
 export default function Map({ locations }) {
 	const mapRef = useRef(null);
 	const tableRef = useRef(null);
+	const featureTable = useRef(null);
 
 	useEffect(() => {
 		const map = new ArcgisMap({
@@ -80,71 +49,94 @@ export default function Map({ locations }) {
 		});
 
 		const featureLayer = new FeatureLayer({
-			fields: fields,
+			fields: [
+				{
+					name: "id",
+					type: "oid",
+				},
+				{
+					name: "name",
+					type: "string",
+				},
+				{
+					name: "address",
+					type: "string",
+				},
+				{
+					name: "locality",
+					type: "string",
+				},
+			],
 			geometryType: "point",
 			spatialReference: { wkid: 4326 },
 			source: graphics,
-			popupTemplate: popupTemplate,
-			renderer: renderer,
+			popupTemplate: {
+				title: "{name}",
+				content: "{address} {locality} AB",
+			},
+			renderer: {
+				type: "simple",
+				symbol: {
+					type: "simple-marker",
+					color: "#8e5776",
+				},
+			},
 		});
 
 		map.add(featureLayer);
 
-		const searchWidget = new Search({
-			view: view,
-		});
+		view.when(() => {
+			const searchWidget = new Search({
+				view: view,
+			});
 
-		view.ui.add(searchWidget, {
-			position: "top-right",
-			index: 2,
-		});
+			view.ui.add(searchWidget, {
+				position: "top-right",
+				index: 0,
+			});
 
-		// Create the feature table
-		const featureTable = new FeatureTable({
-			view: view,
-			layer: featureLayer,
-			container: tableRef.current,
-			visibleElements: {
-				header: false,
-				menu: false,
-				selectionColumn: false,
-				columnMenus: false,
-			},
-			tableTemplate: {
-				columnTemplates: [
-					{
-						fieldName: "name",
-						label: "Clinic",
-						direction: "asc",
-						initialSortPriority: 0,
+			if (featureTable.current == null) {
+				featureTable.current = new FeatureTable({
+					view: view,
+					layer: featureLayer,
+					container: tableRef.current,
+					visibleElements: {
+						header: false,
+						menu: false,
+						selectionColumn: false,
+						columnMenus: false,
 					},
-					{
-						fieldName: "address",
-						label: "Address",
+					tableTemplate: {
+						columnTemplates: [
+							{
+								fieldName: "name",
+								label: "Clinic",
+								direction: "asc",
+								initialSortPriority: 0,
+							},
+							{
+								fieldName: "address",
+								label: "Address",
+							},
+							{
+								fieldName: "locality",
+								label: "Location",
+							},
+						],
 					},
-					{
-						fieldName: "locality",
-						label: "Location",
-					},
-				],
-			},
-		});
-
-		reactiveUtils.when(
-			() => view.stationary,
-			() => {
-				// Filter out and show only the visible features in the feature table.
-				featureTable.filterGeometry = view.extent;
-			},
-			{
-				initial: true,
+				});
 			}
-		);
-
-		return () => {
-			featureTable?.destroy();
-			view?.destroy();
-		};
+			reactiveUtils.when(
+				() => view.stationary,
+				() => {
+					// Filter out and show only the visible features in the feature table.
+					featureTable.filterGeometry = view.extent;
+				},
+				{
+					initial: true,
+				}
+			);
+		});
 	}, [locations]);
 
 	return (
