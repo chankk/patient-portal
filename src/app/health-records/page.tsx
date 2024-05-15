894228c1189ecdf33a9758e921060d75d7bf56e2
chankk <42552752+chankk@users.noreply.github.com>
2024-05-15 11:51:28 -0600

    Remove unimplemented cards from Health Records

--- src/app/health-records/page.tsx
+++ src/app/health-records/page.tsx
@@ -11,37 +11,16 @@ const cardData = [
 		href: "/health-records/test-results",
 	},
 	{
-		heading: "Health Summary",
-		content: "A comprehensive report of your medical history.",
-		href: "",
-	},
-	{
 		heading: "Immunizations",
 		content: "Summary of your immunization history.",
 		href: "/health-records/immunization",
-	},
-	{
-		heading: "Medications",
-		content: "Prescribed medications, their dosage, frequency and other details.",
-		href: "",
-	},
-	{
-		heading: "Allergies",
-		content: "Information on your known allergies and related details.",
-		href: "",
 	},
 ];
 
 const cards = cardData.map((card) => {
 	return (
-		<Grid
-			item
-			key={card.heading}
-			lg={3}>
-			<Card
-				heading={card.heading}
-				content={card.content}
-				href={card.href}></Card>
+		<Grid item key={card.heading} lg={3}>
+			<Card heading={card.heading} content={card.content} href={card.href}></Card>
 		</Grid>
 	);
 });
@@ -50,10 +29,7 @@ export default function HealthRecords() {
 	return (
 		<Box component="main">
 			<Typography variant="h4">Health Records</Typography>
-			<Grid
-				container
-				my={1}
-				spacing={3}>
+			<Grid container my={1} spacing={3}>
 				{cards}
 			</Grid>
 		</Box>
